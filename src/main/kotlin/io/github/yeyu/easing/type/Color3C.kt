ab5ecb92e1d2a31afc0d46ab9ef19abc374a60f6
ye-yu <rafolwen98@gmail.com>
2020-08-30 22:23:11 +0800

    Modify Color4C channel; Add documentation to color class

--- src/main/kotlin/io/github/yeyu/easing/type/Color3C.kt
+++ src/main/kotlin/io/github/yeyu/easing/type/Color3C.kt
@@ -2,7 +2,21 @@ package io.github.yeyu.easing.type
 
 import kotlin.math.round
 
+/**
+ * A type depicting three color channels of a byte size
+ *
+ * The range for each channel must be [0, 255]. When performing calculations,
+ * values out of range may happen. Therefore, it may be helpful to use the
+ * [wrap] or [clip] method to overflow the values.
+ *
+ * The usage of this class is module specific and is not maintained for
+ * other module.
+ * */
 class Color3C(private val c1: Int, private val c2: Int, private val c3: Int, allowOverflow: Boolean = false) : Number() {
+
+    /**
+     * The max value for each channel
+     * */
     private val max = 256
 
     init {
@@ -13,10 +27,16 @@ class Color3C(private val c1: Int, private val c2: Int, private val c3: Int, all
         }
     }
 
+    /**
+     * Condition for channel
+     * */
     private fun requireRange(n: Int, name: String) {
         require(n in 0 until max) { "$name{$n} cannot be out of the range [0, 255]" }
     }
 
+    /**
+     * Performs addition and overflow
+     * */
     fun plusWrap(other: Color3C): Color3C {
         val c1 = (this.c1 + other.c1) % max
         val c2 = (this.c2 + other.c2) % max
@@ -24,18 +44,41 @@ class Color3C(private val c1: Int, private val c2: Int, private val c3: Int, all
         return Color3C(c1, c2, c3)
     }
 
+    /**
+     * Performs subtraction and overflow
+     * */
     fun minusWrap(other: Color3C): Color3C {
         return this.plusWrap(other.negate())
     }
 
+    /**
+     * Performs overflow if value is out of bound
+     * */
     fun wrap(): Color3C {
         return Color3C(c1 % max, c2 % max, c3 % max)
     }
 
+    /**
+     * Clips each channel to the range of [0, 255]
+     * */
+    fun clip(): Color3C {
+        return Color3C(
+            c1.coerceAtMost(max - 1).coerceAtLeast(0),
+            c2.coerceAtMost(max - 1).coerceAtLeast(0),
+            c3.coerceAtMost(max - 1).coerceAtLeast(0)
+        )
+    }
+
+    /**
+     * Negates the value at each channel
+     * */
     private fun negate(): Color3C {
         return Color3C(c1 * -1, c2 * -1, c3 * -1, true)
     }
 
+    /**
+     * Performs channel-wise addition. May produce value out of bound
+     * */
     operator fun plus(other: Color3C): Color3C {
         val c1 = (this.c1 + other.c1)
         val c2 = (this.c2 + other.c2)
@@ -43,6 +86,9 @@ class Color3C(private val c1: Int, private val c2: Int, private val c3: Int, all
         return Color3C(c1, c2, c3, true)
     }
 
+    /**
+     * Performs channel-wise subtraction. May produce value out of bound
+     * */
     operator fun minus(other: Color3C): Color3C {
         val c1 = (this.c1 - other.c1)
         val c2 = (this.c2 - other.c2)
@@ -50,6 +96,9 @@ class Color3C(private val c1: Int, private val c2: Int, private val c3: Int, all
         return Color3C(c1, c2, c3, true)
     }
 
+    /**
+     * Performs multiplication of the magnitude on each channel. May produce value out of bound
+     * */
     operator fun times(other: Int): Color3C {
         val c1 = (this.c1 * other)
         val c2 = (this.c2 * other)
@@ -57,6 +106,9 @@ class Color3C(private val c1: Int, private val c2: Int, private val c3: Int, all
         return Color3C(c1, c2, c3, true)
     }
 
+    /**
+     * Performs multiplication of the magnitude on each channel. May produce value out of bound
+     * */
     operator fun times(other: Double): Color3C {
         val c1 = round(this.c1 * other).toInt()
         val c2 = round(this.c2 * other).toInt()
@@ -64,6 +116,9 @@ class Color3C(private val c1: Int, private val c2: Int, private val c3: Int, all
         return Color3C(c1, c2, c3, true)
     }
 
+    /**
+     * Performs multiplication of the magnitude on each channel. May produce value out of bound
+     * */
     operator fun times(other: Number): Color3C {
         return when (other) {
             is Double -> this * other
@@ -73,6 +128,9 @@ class Color3C(private val c1: Int, private val c2: Int, private val c3: Int, all
         }
     }
 
+    /**
+     * Performs division of the magnitude on each channel. May produce value out of bound
+     * */
     operator fun div(other: Int): Color3C {
         val c1 = (this.c1 / other)
         val c2 = (this.c2 / other)
--- src/main/kotlin/io/github/yeyu/easing/type/Color4C.kt
+++ src/main/kotlin/io/github/yeyu/easing/type/Color4C.kt
@@ -1,20 +1,44 @@
 package io.github.yeyu.easing.type
 
-data class Color4C(private val c1: Int, private val c2: Int, private val c3: Int, private val c4: Int): Number() {
-    init {
-        requireRange(c1, "channel 1")
-        requireRange(c2, "channel 2")
-        requireRange(c3, "channel 3")
-        requireRange(c4, "channel 4")
-    }
-    
+import kotlin.math.round
+
+/**
+ * A type depicting four color channels of a byte size
+ *
+ * The range for each channel must be [0, 255]. When performing calculations,
+ * values out of range may happen. Therefore, it may be helpful to use the
+ * [wrap] or [clip] method to overflow the values.
+ *
+ * The usage of this class is module specific and is not maintained for
+ * other module.
+ * */
+class Color4C(private val c1: Int, private val c2: Int, private val c3: Int, private val c4: Int, allowOverflow: Boolean = false) : Number() {
+
+    /**
+     * The max value for each channel
+     * */
     private val max = 256
 
-    private fun requireRange(n: Int, name: String) {
-        require(n in 0 until max) { "$name cannot be out of the range [0, 255]" }
+    init {
+        if (!allowOverflow) {
+            requireRange(c1, "channel 1")
+            requireRange(c2, "channel 2")
+            requireRange(c3, "channel 3")
+            requireRange(c4, "channel 4")
+        }
     }
 
-    operator fun plus(other: Color4C): Color4C {
+    /**
+     * Condition for channel
+     * */
+    private fun requireRange(n: Int, name: String) {
+        require(n in 0 until max) { "$name{$n} cannot be out of the range [0, 255]" }
+    }
+
+    /**
+     * Performs addition and overflow
+     * */
+    fun plusWrap(other: Color4C): Color4C {
         val c1 = (this.c1 + other.c1) % max
         val c2 = (this.c2 + other.c2) % max
         val c3 = (this.c3 + other.c3) % max
@@ -22,28 +46,104 @@ data class Color4C(private val c1: Int, private val c2: Int, private val c3: Int
         return Color4C(c1, c2, c3, c4)
     }
 
+    /**
+     * Performs subtraction and overflow
+     * */
+    fun minusWrap(other: Color4C): Color4C {
+        return this.plusWrap(other.negate())
+    }
+
+    /**
+     * Performs overflow if value is out of bound
+     * */
+    fun wrap(): Color4C {
+        return Color4C(c1 % max, c2 % max, c3 % max, c4 % max)
+    }
+
+    /**
+     * Clips each channel to the range of [0, 255]
+     * */
+    fun clip(): Color4C {
+        return Color4C(
+            c1.coerceAtMost(max - 1).coerceAtLeast(0),
+            c2.coerceAtMost(max - 1).coerceAtLeast(0),
+            c3.coerceAtMost(max - 1).coerceAtLeast(0),
+            c4.coerceAtMost(max - 1).coerceAtLeast(0)
+        )
+    }
+
+    /**
+     * Negates the value at each channel
+     * */
+    private fun negate(): Color4C {
+        return Color4C(c1 * -1, c2 * -1, c3 * -1, c4 * -1,  true)
+    }
+
+    /**
+     * Performs channel-wise addition. May produce value out of bound
+     * */
+    operator fun plus(other: Color4C): Color4C {
+        val c1 = (this.c1 + other.c1)
+        val c2 = (this.c2 + other.c2)
+        val c3 = (this.c3 + other.c3)
+        val c4 = (this.c4 + other.c4)
+        return Color4C(c1, c2, c3, c4, true)
+    }
+
+    /**
+     * Performs channel-wise subtraction. May produce value out of bound
+     * */
     operator fun minus(other: Color4C): Color4C {
-        val c1 = (this.c1 - other.c1) % max
-        val c2 = (this.c2 - other.c2) % max
-        val c3 = (this.c3 - other.c3) % max
-        val c4 = (this.c4 - other.c4) % max
-        return Color4C(c1, c2, c3, c4)
+        val c1 = (this.c1 - other.c1)
+        val c2 = (this.c2 - other.c2)
+        val c3 = (this.c3 - other.c3)
+        val c4 = (this.c4 - other.c4)
+        return Color4C(c1, c2, c3, c4, true)
     }
 
+    /**
+     * Performs multiplication of the magnitude on each channel. May produce value out of bound
+     * */
     operator fun times(other: Int): Color4C {
-        val c1 = (this.c1 * other) % max
-        val c2 = (this.c2 * other) % max
-        val c3 = (this.c3 * other) % max
-        val c4 = (this.c4 * other) % max
-        return Color4C(c1, c2, c3, c4)
+        val c1 = (this.c1 * other)
+        val c2 = (this.c2 * other)
+        val c3 = (this.c3 * other)
+        val c4 = (this.c4 * other)
+        return Color4C(c1, c2, c3, c4, true)
     }
 
+    /**
+     * Performs multiplication of the magnitude on each channel. May produce value out of bound
+     * */
+    operator fun times(other: Double): Color4C {
+        val c1 = round(this.c1 * other).toInt()
+        val c2 = round(this.c2 * other).toInt()
+        val c3 = round(this.c3 * other).toInt()
+        val c4 = round(this.c4 * other).toInt()
+        return Color4C(c1, c2, c3, c4, true)
+    }
+
+    /**
+     * Performs multiplication of the magnitude on each channel. May produce value out of bound
+     * */
+    operator fun times(other: Number): Color4C {
+        return when (other) {
+            is Double -> this * other
+            is Int -> this * other
+            is Float -> this * other.toDouble()
+            else -> throw IllegalArgumentException("Convert to double or int first!")
+        }
+    }
+
+    /**
+     * Performs division of the magnitude on each channel. May produce value out of bound
+     * */
     operator fun div(other: Int): Color4C {
-        val c1 = (this.c1 / other) % max
-        val c2 = (this.c2 / other) % max
-        val c3 = (this.c3 / other) % max
-        val c4 = (this.c4 / other) % max
-        return Color4C(c1, c2, c3, c4)
+        val c1 = (this.c1 / other)
+        val c2 = (this.c2 / other)
+        val c3 = (this.c3 / other)
+        val c4 = (this.c4 / other)
+        return Color4C(c1, c2, c3, c4, true)
     }
 
     @Deprecated("Can only convert to Int")
@@ -67,7 +167,7 @@ data class Color4C(private val c1: Int, private val c2: Int, private val c3: Int
     }
 
     override fun toInt(): Int {
-        return c1 shl 24 + c2 shl 16 + c3 shl 8 + c4
+        return c1 shl 16 + c2 shl 8 + c3
     }
 
     @Deprecated("Can only convert to Int")
@@ -78,5 +178,20 @@ data class Color4C(private val c1: Int, private val c2: Int, private val c3: Int
     @Deprecated("Can only convert to Int")
     override fun toShort(): Short {
         throw IllegalAccessError("Cannot convert color to short")
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other is Color4C) {
+            return other.c1 == this.c1 && other.c2 == this.c2 && other.c3 == this.c3 && other.c4 == this.c4
+        }
+        return super.equals(other)
+    }
+
+    override fun hashCode(): Int {
+        var result = c1
+        result = 31 * result + c2
+        result = 31 * result + c3
+        result = 31 * result + c4
+        return result
     }
 }
