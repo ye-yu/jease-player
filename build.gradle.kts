23aa06b3399f6e9997ad4a9187b157bf9c1304ea
ye-yu <rafolwen98@gmail.com>
2020-08-30 20:40:10 +0800

    Add publishing to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,23 @@
+@file:Suppress("UNUSED_VARIABLE")
+
 plugins {
+    java
+    id("org.jetbrains.dokka") version "1.4.0-rc"
     kotlin("jvm") version Jetbrains.Kotlin.version
     kotlin("plugin.serialization") version Jetbrains.Kotlin.version
     `maven-publish`
+    id("maven")
+    id("signing")
 }
 
 group = Info.group
 version = Info.version
+val sonatypeUsername: String by project
+val sonatypePassword: String by project
 
 repositories {
-    maven(url = "https://kotlin.bintray.com/kotlinx/") { name = "Kotlinx" }
-    mavenCentral()
     jcenter()
+    mavenCentral()
 }
 
 dependencies {
@@ -32,10 +39,7 @@ tasks {
 
     val javadocJar by creating(Jar::class) {
         archiveClassifier.set("javadoc")
-
-        from(project.tasks["javadoc"])
-
-        dependsOn(JavaPlugin.JAVADOC_TASK_NAME)
+        from(project.tasks["dokkaJavadoc"])
     }
 
     compileJava {
@@ -63,8 +67,83 @@ publishing {
             }
         }
 
+        create<MavenPublication>("mavenLocal") {
+            artifacts {
+                version += "-LOCAL"
+
+                artifact(tasks["sourcesJar"])
+                artifact(tasks["javadocJar"])
+            }
+        }
+
         repositories {
             mavenLocal()
+        }
+    }
+}
+
+tasks.withType(PublishToMavenLocal::class) {
+    onlyIf {
+        publication == publishing.publications["mavenLocal"]
+    }
+}
+
+tasks.withType(PublishToMavenRepository::class) {
+    onlyIf {
+        publication != publishing.publications["mavenLocal"]
+    }
+}
+
+tasks.named<Upload>("uploadArchives") {
+    repositories {
+        withConvention(MavenRepositoryHandlerConvention::class) {
+            mavenDeployer {
+                beforeDeployment {
+                    this.addArtifact(signing.sign(this.pomArtifact).singleSignature.apply {
+                        this.type = "pom." + this.signatureType.extension
+                    })
+                }
+
+                withGroovyBuilder {
+                    "repository"("url" to "https://oss.sonatype.org/service/local/staging/deploy/maven2/") {
+                        "authentication"("userName" to sonatypeUsername, "password" to sonatypePassword)
+                    }
+
+                    "snapshotRepository"("url" to "https://oss.sonatype.org/content/repositories/snapshots/") {
+                        "authentication"("userName" to sonatypeUsername, "password" to sonatypePassword)
+                    }
+                }
+                pom.project {
+                    withGroovyBuilder {
+                        "name"("Jeasing")
+                        "packaging"("jar")
+                        // optionally artifactId can be defined here
+                        "description"("Java Easing Player Library")
+                        "url"("https://github.com/ye-yu/jease-player/tree/master")
+
+                        "scm" {
+                            "connection"("scm:git:ssh://git@github.com:ye-yu/jease-player.git")
+                            "developerConnection"("scm:git:ssh://git@github.com:ye-yu/jease-player.git")
+                            "url"("https://git@github.com:ye-yu/jease-player.git")
+                        }
+
+                        "licenses" {
+                            "license" {
+                                setProperty("name", "GPLv3")
+                                setProperty("url", "https://github.com/ye-yu/jease-player/blob/master/LICENSE")
+                            }
+                        }
+
+                        "developers" {
+                            "developer" {
+                                setProperty("id", "ye-yu")
+                                setProperty("name", "Ye Yu")
+                                setProperty("email", "rafolwen98@gmail.com")
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
