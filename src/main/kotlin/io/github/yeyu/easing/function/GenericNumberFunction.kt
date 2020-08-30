2fc98118ab9e340bc169429893c4acab54ad2ff6
ye-yu <rafolwen98@gmail.com>
2020-08-30 14:18:28 +0800

    Remove unused generic function

--- src/main/kotlin/io/github/yeyu/easing/function/GenericNumberFunction.kt
+++
@@ -1,5 +0,0 @@
-package io.github.yeyu.easing.function
-
-interface GenericNumberFunction<T: Number> {
-    fun f(x: T): T
-}
--- src/main/kotlin/io/github/yeyu/easing/function/LinearGenericFunction.kt
+++
@@ -1,9 +0,0 @@
-package io.github.yeyu.easing.function
-
-object LinearGenericFunction {
-    operator fun <T: Number> invoke(): GenericNumberFunction<T> {
-        return object: GenericNumberFunction<T> {
-            override fun f(x: T): T = x
-        }
-    }
-}
